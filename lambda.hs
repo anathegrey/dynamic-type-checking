d09845a81a6f3db13905c8bee137ce47681bf3b7
anathegrey <anajorgecsoaresalmeida@gmail.com>
2020-02-26 11:47:32 +0000

    first draft

--- lambda.hs
+++ lambda.hs
@@ -15,30 +15,30 @@ remove _ [] = []
 remove y (x : xs) = if y == x then remove y xs else [x] ++ remove y xs
 
 free :: Term -> [String]
-free (Var x) = [x]
+free (Var x) = [x] --corrigir
 free (Func x m) = remove x (free m)
 free (App m n) = free m ++ free n
 
---substitution
+--substitution (rever)
+subs :: Term -> Term -> String -> Term
+subs (Var x) l y = if x == y then l else (Var x)
+subs (Func x m) l y = if x == y then (Func x m) else (Func x (subs m l y))
+subs (App m n) l y = App (subs m l y) (subs n l y)
+
+--beta-conversion (rever)
 intersect :: [String] -> [String] -> [String]
 intersect [] _ = []
 intersect (x : xs) y
 	  | elem x y = [x] ++ intersect xs y
 	  | otherwise = intersect xs y
 
-subs :: Term -> Term -> String -> Term
-subs (Var x) l y = if x == y then l else (Var x)
-subs (Func x m) l y = if x == y then (Func x m) else (Func x (subs m l y))
-subs (App m n) l y = App (subs m l y) (subs n l y)
-
---beta-conversion
 betaConv :: Term -> Term
 betaConv (Var x) = Var x
 betaConv (Func x m) = Func x m
 betaConv (App (Func x m) n) = if (intersect (bound (Func x m)) (free n)) == [] then subs m n x else (App (Func x m) n)
 
 
---reductions
+--reductions (incompleto)
 reduction :: Term -> Term
 reduction (Var x) = Var x
 reduction (Func x m) = Func x m
